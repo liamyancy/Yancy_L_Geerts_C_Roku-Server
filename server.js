0e598b67895a4c10de5edf848a45b7810e344c29
cgeerts <55294018+cgeerts@users.noreply.github.com>
2021-04-15 00:52:33 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+const port = process.env.PORT || 5050;
+
 const app = express();
 
-const port = process.env.PORT || 5000;
+app.use(cookieParser());
+
+app.use(session({
+    
+}))
 
 app.use((req, res, next) => {
     console.log('incoming request');
@@ -10,12 +19,28 @@ app.use((req, res, next) => {
     next();
 })
 
+app.use("/user", require("./routes/ums"));
+app.use("/api", require("./routes/api"));
+app.use("/ums", require("./routes/ums"));
+
 app.listen(port, () => {
     console.log(`server is running on ${port}`);
 })
 
-app.use("/api", require("./routes/api"));
+// const express = require('express');
+// const app = express();
+
+// const port = process.env.PORT || 5050;
+
+// app.use((req, res, next) => {
+//     console.log('incoming request');
+//     console.log(port);
+
+//     next();
+// })
 
 // app.listen(port, () => {
 //     console.log(`server is running on ${port}`);
 // })
+
+// app.use("/api", require("./routes/api"));
